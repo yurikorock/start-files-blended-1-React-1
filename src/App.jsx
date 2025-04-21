56e2b0c3ae03335ec90421579ad42a6eebdf49e8
Yura <yurikorock777@gmail.com>
2025-04-21 10:42:47 +0300

    add task 3

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Statistics from './components/Statistics/Statistics';
 
 import article from './data/article.json';
 import stats from './data/stats.json';
+import forbes from './data/forbes.json';
 
 export const App = () => {
   return (
@@ -27,11 +28,11 @@ export const App = () => {
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics stats={stats} />
 
-        {/* <Heading title="Task 3 Forbes list" top bottom /> */}
-        {/* <ForbesList /> */}
+        <Heading title="Task 3 Forbes list" top bottom />
+        <ForbesList list={forbes} />
 
         {/* <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory /> */}
+        <CryptoHistory />  */}
       </Container>
     </Section>
   );
